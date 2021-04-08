924d2e4c0e699a776ea154148314cd20e0bc5550
Giancarlo Danves <gdanves@gmail.com>
2021-04-08 11:12:48 -0300

    adjusting background color

--- scripts/start-screen.js
+++ scripts/start-screen.js
@@ -4,7 +4,6 @@
 class StartScreen {
     constructor(onNewGameClick) {
         this.startScreen = document.querySelector("#start-screen");
-        document.body.style.background = "#ffbea7"
         this.startScreen.style.display = "block";
         this.onNewGameClick = onNewGameClick;
         this.button = document.querySelector("#start-game");
